b2d402015883b8ca109fe3e00b8f3688317eb4c8
GORIKAPUDI TRINADH <gtrinadh2007@gmail.com>
2026-08-31 23:43:18 +0530

    Time: 2 ms (91.87%), Space: 47.9 MB (38.36%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int left=0;
+        
+        for(int right=0;right<n;right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+                
+            }
+        }
+        }
+}
