b23ba5c309d1b8982ad75a169ce232be64ca6de8
GORIKAPUDI TRINADH <gtrinadh2007@gmail.com>
2026-09-04 14:00:30 +0530

    Time: 1 ms (92.36%), Space: 43.2 MB (21.32%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,16 +1,12 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int n=nums.length;
-        int count=0;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(nums[i]>0 && (i==0 || nums[i] != nums[i-1])){
-                count++;
+       HashSet<Integer> a=new HashSet<>();
+       for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                a.add(nums[i]);
             }
-
-
-        }
-        return count;
+       }
+        return a.size();
         
     }
 }
