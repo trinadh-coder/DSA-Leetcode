bbc2205401b7d01aabc462f4e3483533158346ef
GORIKAPUDI TRINADH <gtrinadh2007@gmail.com>
2026-09-07 21:50:19 +0530

    Time: 0 ms (100%), Space: 42 MB (52.38%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n-1;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
