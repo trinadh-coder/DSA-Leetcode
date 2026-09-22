2efa384915efbadfe0c2d7304f8c8cd20a9de7b4
GORIKAPUDI TRINADH <gtrinadh2007@gmail.com>
2026-09-22 19:30:46 +0530

    Time: 0 ms (100%), Space: 47.2 MB (81.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[0] != 0) {
-            return 0;
-        }
-        int l=0,r=1;
-       while(r<nums.length){
-        if(nums[l]+1==nums[r]){
-            l++;
-        r++;
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2; 
+        
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
         }
         
-        else{
-        return nums[l]+1;
-        }
-
-       }
-       return nums[nums.length-1]+1;
+        return expectedSum - actualSum;
     }
 }
