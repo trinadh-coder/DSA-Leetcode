89702eef28aaf5ee35c1ed95c1ce972921881c2a
GORIKAPUDI TRINADH <gtrinadh2007@gmail.com>
2026-09-25 11:35:14 +0530

    Time: 2 ms (72.57%), Space: 63.3 MB (6.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,12 +3,11 @@ class Solution {
         int candidate = 0;
         int count = 0;
         
-        for (int num : nums) {
+        for (int i = 0; i < nums.length; i++) {
             if (count == 0) {
-                candidate = num;
+                candidate = nums[i];
             }
-            
-            if (num == candidate) {
+            if (nums[i] == candidate) {
                 count++;
             } else {
                 count--;
